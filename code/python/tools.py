47278284134cf50cbcb8c2d22fc506ea754cc864
Rawser Spicer <rwspicer@alaska.edu>
2022-03-28 12:05:50 -0800

    fixes bugs in calcualtions fof julian days and reflectance

--- code/python/tools.py
+++ code/python/tools.py
@@ -13,10 +13,15 @@ import satellites
 import numpy as np
 
 def digiglobe_find_date (p, pf):
-    """get the data from a digital globe path
+    """get the date from a digital globe path
     
     Parameters
     ----------
+    p: path 
+        path to a image file with path format from digital globe compressed data
+    pf: str
+        use '%s'
+
     
     """
     return datetime.strptime(
@@ -63,21 +68,23 @@ def calc_julian_days_dg(tlc_time):
         days since the beginning of the year -4712 
         Meuss, Jean. "Astronomical algorithms, 2nd Ed.." Richmond, VA: Willmann-Bell(1998). Pg 61
     """
-    a = tlc_time.year//100 
-    b = 2 - a + a // 4
-    UT = tlc_time.hour + tlc_time.minute/60 + (tlc_time.second+tlc_time.microsecond)/3600
-    jd = int(365.25*tlc_time.year+4716) + \
+    a = tlc_time.year // 100 
+    b = 2 - a + (a // 4)
+    UT = tlc_time.hour + tlc_time.minute/60 + \
+        (tlc_time.second+tlc_time.microsecond)/3600
+    jd = int(365.25*(tlc_time.year+4716)) + \
         int(30.6001*(tlc_time.month+1)) + \
         tlc_time.day + UT/24+b-1524.5
     return jd
 
 def calc_dist_sun_earth_au(jd):
-    """Caclulate the earth sun distance in AU. Found in section 4.1.2 
+    """Calculate the earth sun distance in AU. Found in section 4.1.2 
     here https://dg-cms-uploads-production.s3.amazonaws.com/uploads/document/file/207/Radiometric_Use_of_WorldView-3_v2.pdf
 
     Parameters
     ----------
-    jd: int in julian days
+    jd: int 
+        days in julian days
 
     Returns
     -------
@@ -86,6 +93,7 @@ def calc_dist_sun_earth_au(jd):
     """
     d = jd - 2451545.0
     g = 357.529 + 0.98560028 * d
-    d_es = 1.00014 - 0.01671 * np.cos(g) - 0.00014*np.cos(2*g)
+    d_es = 1.00014 - 0.01671 * np.cos( np.radians(g) ) - \
+        0.00014 * np.cos( np.radians(2*g) )
     return d_es
 
